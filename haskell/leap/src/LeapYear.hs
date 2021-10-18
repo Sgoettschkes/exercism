12f0bc97941ee184aed70b37540bbc98feea4f75
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2021-10-18 21:37:06 +0200

    Move code for haskell/leap into correct folder

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,6 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year = yearModBy 400 || (yearNotModBy 100 && yearModBy 4)
+    where yearModBy i = year `mod` i == 0
+          yearNotModBy = not . yearModBy
