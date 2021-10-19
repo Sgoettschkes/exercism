a42ef8e9ccb0ce00a3b5ad91da80b11817c075a0
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2021-10-19 21:09:25 +0200

    Improve solution for haskell/sum-of-multiplies

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,6 +1,6 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples [3, 0] x = 3
 sumOfMultiples multipliers x = sum $ filter isMultipleOfAny [1..(x-1)]
-    where isMultipleOfAny num = any (\x -> num `mod` x == 0) multipliers
+    where m = filter (/= 0) multipliers
+          isMultipleOfAny num = any (\y -> num `mod` y == 0) m
