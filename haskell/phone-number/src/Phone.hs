7e9c3c100a632b12623115b0f70fcaf2c6ec84b8
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2022-01-02 14:55:41 +0100

    Provide version of haskell/phone-number without Regex

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,12 +1,21 @@
 module Phone (number) where
 
-import Text.Regex.Posix
-
 number :: String -> Maybe String
 number xs = res
-  where realNum = filter (`elem` ['0'..'9']) xs
+  where fillNum = filter (`elem` ['0'..'9']) xs
+        numWithoutCountryCode = drop (length fillNum - 10) fillNum
         res
-          | realNum =~ "^[2-9][0-9]{2}[2-9][0-9]{2}[0-9]{4}$" = Just realNum
-          | realNum =~ "^1[2-9][0-9]{2}[2-9][0-9]{2}[0-9]{4}$" = Just $ drop 1 realNum 
+          | correctCountryCode fillNum && correctAreaCode numWithoutCountryCode && correctExchangeCode numWithoutCountryCode = Just numWithoutCountryCode
           | otherwise = Nothing
         
+correctCountryCode :: String -> Bool 
+correctCountryCode xs
+  | length xs == 11 && head xs == '1' = True
+  | length xs == 10 = True 
+  | otherwise = False
+
+correctAreaCode :: String -> Bool 
+correctAreaCode xs = head xs `elem` ['2'..'9']
+
+correctExchangeCode :: String -> Bool 
+correctExchangeCode xs = (xs !! 3) `elem` ['2'..'9']
