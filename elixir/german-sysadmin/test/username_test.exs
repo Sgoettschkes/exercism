abc239024dce9d195ff46cbfb1108ead02c0fd7a
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-09-28 20:02:18 +0200

    Update elixir and erlang and use asdf

--- elixir/german-sysadmin/test/username_test.exs
+++ elixir/german-sysadmin/test/username_test.exs
@@ -4,13 +4,13 @@ defmodule UsernameTest do
   describe "sanitize/1" do
     @tag task_id: 1
     test "works for an empty charlist" do
-      assert Username.sanitize('') == ''
+      assert Username.sanitize(~c"") == ~c""
     end
 
     @tag task_id: 1
     test "it keeps lowercase latin letters" do
       input = Enum.to_list(0..0x10FFFF) -- [?_, ?ß, ?ä, ?ö, ?ü]
-      expected = 'abcdefghijklmnopqrstuvwxyz'
+      expected = ~c"abcdefghijklmnopqrstuvwxyz"
       actual = Username.sanitize(input)
 
       assert Enum.take(actual, Enum.count(expected)) == expected
@@ -19,15 +19,15 @@ defmodule UsernameTest do
 
     @tag task_id: 2
     test "it keeps underscores" do
-      assert Username.sanitize('marcel_huber') == 'marcel_huber'
+      assert Username.sanitize(~c"marcel_huber") == ~c"marcel_huber"
     end
 
     @tag task_id: 3
     test "it substitutes german letters" do
-      assert Username.sanitize('krüger') == 'krueger'
-      assert Username.sanitize('köhler') == 'koehler'
-      assert Username.sanitize('jäger') == 'jaeger'
-      assert Username.sanitize('groß') == 'gross'
+      assert Username.sanitize(~c"krüger") == ~c"krueger"
+      assert Username.sanitize(~c"köhler") == ~c"koehler"
+      assert Username.sanitize(~c"jäger") == ~c"jaeger"
+      assert Username.sanitize(~c"groß") == ~c"gross"
     end
   end
 end
