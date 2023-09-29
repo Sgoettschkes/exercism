f7a7201884cfa70f246b6962e1c47d4fb04dfa25
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-09-29 07:33:14 +0200

    Update language list in elixir

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -4,7 +4,7 @@ defmodule LanguageList do
   end
 
   def add(list, language) do
-    [language] ++ list
+    [language | list]
   end
 
   def remove([_head | tail]) do
@@ -18,7 +18,7 @@ defmodule LanguageList do
   def count([]), do: 0
   def count([_head | tail]), do: 1 + count(tail)
 
-  def exciting_list?([]), do: false
-  def exciting_list?([head | _tail]) when head == "Elixir", do: true
-  def exciting_list?([_head | tail]), do: exciting_list?(tail)
+  def functional_list?([]), do: false
+  def functional_list?([head | _tail]) when head == "Elixir", do: true
+  def functional_list?([_head | tail]), do: functional_list?(tail)
 end
