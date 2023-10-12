dd8c03a0f4dcaf2053b758687fac3ac35a72af89
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-10-12 09:59:35 +0200

    Update hello-world in haskell

--- haskell/hello-world/test/Tests.hs
+++ haskell/hello-world/test/Tests.hs
@@ -1,13 +1,11 @@
 import Test.Hspec        (Spec, it, shouldBe)
-import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
 import HelloWorld (hello)
 
 main :: IO ()
-main = hspecWith defaultConfig {configFastFail = True} specs
+main = hspecWith defaultConfig {configFailFast = True} specs
 
 specs :: Spec
 specs = it "hello" $
           hello `shouldBe` "Hello, World!"
-
--- 5c542864b011fb742aa95ca950d3473ce168ec8c
