472911aee85aa1483bc0cdcb1251764f60c5ed6e
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-10-12 08:17:05 +0200

    Update rna-transcription in Haskell

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,13 +1,11 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA y 
-  | all (`elem` "ACGT") y = Right $ map dnaRnaMapper y
-  | otherwise = Left $ head $ filter (not . (`elem` "ACGT")) y
-
-dnaRnaMapper :: Char -> Char
-dnaRnaMapper 'G' = 'C'
-dnaRnaMapper 'C' = 'G'
-dnaRnaMapper 'T' = 'A'
-dnaRnaMapper 'A' = 'U'
-dnaRnaMapper _ = error ""
+toRNA = traverse fromDNA
+  where
+    fromDNA :: Char -> Either Char Char
+    fromDNA 'G' = pure 'C'
+    fromDNA 'C' = pure 'G'
+    fromDNA 'T' = pure 'A'
+    fromDNA 'A' = pure 'U'
+    fromDNA c = Left c
