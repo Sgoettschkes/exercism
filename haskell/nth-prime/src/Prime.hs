aabcaab09a2f162e915dfdcece320ac0ab2524c1
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-10-13 12:24:53 +0200

    Make nth-prime in Haskell fast

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -2,14 +2,10 @@ module Prime (nth) where
 
 nth :: Int -> Maybe Integer
 nth 0 = Nothing 
-nth n = Just (primes !! (n - 1))
+nth n = Just $ primes !! (n - 1)
 
 primes :: [Integer]
-primes = filter isPrime [2..]
-
-isPrime :: Integer -> Bool 
-isPrime num
-  | num <= 1 = False
-  | num == 2 = True
-  | even num = False
-  | otherwise = all (\x -> num `mod` x /= 0) [3,5..num-1]
+primes = sieve [2..]
+    where
+        sieve [] = []
+        sieve (p:ps) = p : sieve [x | x <- ps, mod x p /= 0]
