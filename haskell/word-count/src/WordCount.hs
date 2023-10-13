6b85e99abd336dc39aeb01a98b57b4755e2f4ef6
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-10-13 12:34:33 +0200

    Update word-count in Haskell

---
+++ haskell/word-count/src/WordCount.hs
@@ -0,0 +1,15 @@
+module WordCount (wordCount) where
+
+import Data.Char (isAlphaNum, toLower)
+import Data.List (dropWhileEnd)
+import Data.Map (Map, fromListWith)
+
+wordCount :: String -> Map String Int
+wordCount = fromListWith (+) . wordPairs
+    where wordPairs = flip zip (repeat 1) . extractWords
+
+extractWords :: String -> [String]
+extractWords = map stripWord . words . map toLowerCaseOrSpace
+    where 
+        stripWord = dropWhile (not.isAlphaNum) . dropWhileEnd (not.isAlphaNum)
+        toLowerCaseOrSpace x = if (isAlphaNum x) || x == '\'' then toLower x else ' '
