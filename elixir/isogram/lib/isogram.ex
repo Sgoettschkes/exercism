f30aa1bbe933ae6a0a24d53fbcd1f76f4dcf4c6f
Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
2023-11-05 08:44:42 +0100

    Implement isogram in Elixir

---
+++ elixir/isogram/lib/isogram.ex
@@ -0,0 +1,14 @@
+defmodule Isogram do
+  @doc """
+  Determines if a word or sentence is an isogram
+  """
+  @spec isogram?(String.t()) :: boolean
+  def isogram?(sentence) do
+    clean = sentence
+    |> String.downcase()
+    |> String.codepoints()
+    |> Enum.reject(fn char -> char == "-" || char == " " end)
+
+    length(clean) == length(Enum.uniq(clean))
+  end
+end
